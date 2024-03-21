268ff40dfeec3d59574dddfdfabc497987ca3bdc
Leonardo Gehr <leonardo.gehr@pucpr.edu.br>
2024-03-20 22:45:41 -0300

    adicao sets e gets

--- src/Usuario.java
+++ src/Usuario.java
@@ -13,5 +13,43 @@ public class Usuario {
         this.genero = genero;
     }
 
-    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getRg() {
+        return rg;
+    }
+
+    public void setRg(int rg) {
+        this.rg = rg;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
 }
